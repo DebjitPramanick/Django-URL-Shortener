33503cd815e382dc611911d2d118a16ba8e0e73e
DebjitPramanick <dpramanick9990@gmail.com>
2021-04-26 08:15:57 +0530

    Links stored in db

--- urlshortener/shortener/views.py
+++ urlshortener/shortener/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Url
+from django.http import HttpResponse
+import json
+import uuid
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        url = data['url']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=url, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
